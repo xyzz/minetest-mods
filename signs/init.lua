2e90ac3c5ceed62d5dc0dc49b099a5b0c90d4275
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2012-08-08 00:27:41 +0400

    Signs mod almost finished

--- signs/init.lua
+++ signs/init.lua
@@ -24,17 +24,55 @@ end
 print(dump(charmap))
 
 local metas = {"line1", "line2", "line3", "line4", "line5", "line6", "line7"}
+local signs = {
+    ["2"] = {nodebox = {0.4, -0.15, -0.45, 0.498, 0.45, 0.45}, delta = {x = 0.399, y = 0, z = 0}, yaw = math.pi / -2},
+    ["3"] = {nodebox = {-0.498, -0.15, -0.45, -0.4, 0.45, 0.45}, delta = {x = -0.399, y = 0, z = 0}, yaw = math.pi / 2},
+    ["4"] = {nodebox = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}, delta = {x = 0, y = 0, z = 0.399}, yaw = 0},
+    ["5"] = {nodebox = {-0.45, -0.15, -0.498, 0.45, 0.45, -0.4}, delta = {x = 0, y = 0, z = -0.399}, yaw = math.pi},
+}
+
+for param2 in pairs(signs) do
+    local definition = signs[param2]
+    minetest.register_node("signs:sign_"..param2, {
+        drawtype = "nodebox",
+        drop = "signs:sign",
+        tiles = {"default_wood.png"},
+        node_box = {type = "fixed", fixed = definition.nodebox},
+        selection_box = {type = "fixed", fixed = definition.nodebox},
+        sunlight_propagates = true,
+        paramtype = "light",
+        walkable = false,
+
+        on_construct = function(pos)
+            local meta = minetest.env:get_meta(pos)
+            meta:set_string("formspec", "size[7,8]"..
+                "field[1,0;6,3;line1;;${line1}]"..
+                "field[1,1;6,3;line2;;${line2}]"..
+                "field[1,2;6,3;line3;;${line3}]"..
+                "field[1,3;6,3;line4;;${line4}]"..
+                "field[1,4;6,3;line5;;${line5}]"..
+                "field[1,5;6,3;line6;;${line6}]"..
+                "field[1,6;6,3;line7;;${line7}]")
+        end,
+        on_receive_fields = function(pos, formname, fields, sender)
+            local meta = minetest.env:get_meta(pos)
+            local text = {}
+            for i = 1, #metas do
+                table.insert(text, fields[metas[i]])
+                meta:set_string(metas[i], fields[metas[i]])
+            end
+            local object = minetest.env:get_objects_inside_radius(pos, 0.5)[1]
+            object:set_properties({textures={generate_texture(text)}})
+        end,
+    })
+end
 
 minetest.register_node("signs:sign", {
     description = "Sign",
-    drawtype = "nodebox",
     inventory_image = "default_sign_wall.png",
+    wield_image = "default_sign_wall.png",
     stack_max = 1,
-    tiles = {"default_wood.png"},
-    node_box = {type = "fixed", fixed = {-0.498, -0.15, -0.45, -0.4, 0.45, 0.45}},
-    selection_box = {type = "fixed", fixed = {-0.498, -0.15, -0.45, -0.4, 0.45, 0.45}},
-    sunlight_propagates = true,
-    paramtype = "light",
+    node_placement_prediction = "",
 
     on_place = function(itemstack, placer, pointed_thing)
         local above = pointed_thing.above
@@ -43,35 +81,21 @@ minetest.register_node("signs:sign", {
                      y = under.y - above.y, 
                      z = under.z - above.z}
 
-        minetest.env:add_node(above, {name="signs:sign"})
-       
-        print(minetest.dir_to_wallmounted(dir))
+        local wdir = minetest.dir_to_wallmounted(dir)
 
-        local text = minetest.env:add_entity({x=above.x-0.399, y=above.y, z=above.z}, "signs:text")
-        text:setyaw(math.pi/2)
+        if wdir < 2 then
+            --top/bottom not implemented =(
+            minetest.env:add_item(above, "signs:sign")
+        else
+            minetest.env:add_node(above, {name="signs:sign_"..wdir})
+
+            local text = minetest.env:add_entity({x = above.x + signs[wdir..""].delta.x, 
+                                                  y = above.y + signs[wdir..""].delta.y,
+                                                  z = above.z + signs[wdir..""].delta.z}, "signs:text")
+            text:setyaw(signs[wdir..""].yaw)
+        end
 
         return ItemStack("")
-    end,
-    on_construct = function(pos)
-        local meta = minetest.env:get_meta(pos)
-        meta:set_string("formspec", "size[7,8]"..
-            "field[1,0;6,3;line1;;${line1}]"..
-            "field[1,1;6,3;line2;;${line2}]"..
-            "field[1,2;6,3;line3;;${line3}]"..
-            "field[1,3;6,3;line4;;${line4}]"..
-            "field[1,4;6,3;line5;;${line5}]"..
-            "field[1,5;6,3;line6;;${line6}]"..
-            "field[1,6;6,3;line7;;${line7}]")
-    end,
-    on_receive_fields = function(pos, formname, fields, sender)
-        local meta = minetest.env:get_meta(pos)
-        local text = {}
-        for i = 1, #metas do
-            table.insert(text, fields[metas[i]])
-            meta:set_string(metas[i], fields[metas[i]])
-        end
-        local object = minetest.env:get_objects_inside_radius(pos, 0.5)[1]
-        object:set_properties({textures={generate_texture(text)}})
     end,
 })
 
