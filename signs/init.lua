e7171a5f38a598685cb991ca5202f644537981f2
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2012-08-08 20:48:03 +0400

    Finished (?) signs mod

--- signs/init.lua
+++ signs/init.lua
@@ -1,5 +1,14 @@
 -- Font: 04.jp.org
 
+minetest.register_craft({
+    output = "signs:sign",
+    recipe = {
+        {"default:wood", "default:wood", "default:wood"},
+        {"default:wood", "default:wood", "default:wood"},
+        {"", "", ""}
+    }
+})
+
 -- load characters map
 local chars_file = io.open(minetest.get_modpath("signs").."/characters", "r")
 local charmap = {}
@@ -21,51 +30,13 @@ else
     end
 end
 
-print(dump(charmap))
-
 local metas = {"line1", "line2", "line3", "line4", "line5", "line6", "line7"}
 local signs = {
-    ["2"] = {nodebox = {0.4, -0.15, -0.45, 0.498, 0.45, 0.45}, delta = {x = 0.399, y = 0, z = 0}, yaw = math.pi / -2},
-    ["3"] = {nodebox = {-0.498, -0.15, -0.45, -0.4, 0.45, 0.45}, delta = {x = -0.399, y = 0, z = 0}, yaw = math.pi / 2},
-    ["4"] = {nodebox = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}, delta = {x = 0, y = 0, z = 0.399}, yaw = 0},
-    ["5"] = {nodebox = {-0.45, -0.15, -0.498, 0.45, 0.45, -0.4}, delta = {x = 0, y = 0, z = -0.399}, yaw = math.pi},
+    {delta = {x = 0, y = 0, z = 0.399}, yaw = 0},
+    {delta = {x = 0.399, y = 0, z = 0}, yaw = math.pi / -2},
+    {delta = {x = 0, y = 0, z = -0.399}, yaw = math.pi},
+    {delta = {x = -0.399, y = 0, z = 0}, yaw = math.pi / 2},
 }
-
-for param2 in pairs(signs) do
-    local definition = signs[param2]
-    minetest.register_node("signs:sign_"..param2, {
-        drawtype = "nodebox",
-        drop = "signs:sign",
-        tiles = {"default_wood.png"},
-        node_box = {type = "fixed", fixed = definition.nodebox},
-        selection_box = {type = "fixed", fixed = definition.nodebox},
-        sunlight_propagates = true,
-        paramtype = "light",
-        walkable = false,
-
-        on_construct = function(pos)
-            local meta = minetest.env:get_meta(pos)
-            meta:set_string("formspec", "size[7,8]"..
-                "field[1,0;6,3;line1;;${line1}]"..
-                "field[1,1;6,3;line2;;${line2}]"..
-                "field[1,2;6,3;line3;;${line3}]"..
-                "field[1,3;6,3;line4;;${line4}]"..
-                "field[1,4;6,3;line5;;${line5}]"..
-                "field[1,5;6,3;line6;;${line6}]"..
-                "field[1,6;6,3;line7;;${line7}]")
-        end,
-        on_receive_fields = function(pos, formname, fields, sender)
-            local meta = minetest.env:get_meta(pos)
-            local text = {}
-            for i = 1, #metas do
-                table.insert(text, fields[metas[i]])
-                meta:set_string(metas[i], fields[metas[i]])
-            end
-            local object = minetest.env:get_objects_inside_radius(pos, 0.5)[1]
-            object:set_properties({textures={generate_texture(text)}})
-        end,
-    })
-end
 
 minetest.register_node("signs:sign", {
     description = "Sign",
@@ -73,29 +44,70 @@ minetest.register_node("signs:sign", {
     wield_image = "default_sign_wall.png",
     stack_max = 1,
     node_placement_prediction = "",
+    paramtype = "light",
+    paramtype2 = "facedir",
+    drawtype = "nodebox",
+    node_box = {type = "fixed", fixed = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}},
+    selection_box = {type = "fixed", fixed = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}},
+    tiles = {"default_wood.png"},
+    walkable = false,
+    groups = {choppy=2, dig_immediate=2},
 
     on_place = function(itemstack, placer, pointed_thing)
         local above = pointed_thing.above
         local under = pointed_thing.under
-        local dir = {x = under.x - above.x, 
-                     y = under.y - above.y, 
+        local dir = {x = under.x - above.x,
+                     y = under.y - above.y,
                      z = under.z - above.z}
 
-        local wdir = minetest.dir_to_wallmounted(dir)
+        local fdir = minetest.dir_to_facedir(dir) + 1
 
-        if wdir < 2 then
+        if minetest.dir_to_wallmounted(dir) < 2 then
             --top/bottom not implemented =(
             minetest.env:add_item(above, "signs:sign")
         else
-            minetest.env:add_node(above, {name="signs:sign_"..wdir})
+            minetest.env:add_node(above, {name="signs:sign", param2 = fdir - 1})
 
-            local text = minetest.env:add_entity({x = above.x + signs[wdir..""].delta.x, 
-                                                  y = above.y + signs[wdir..""].delta.y,
-                                                  z = above.z + signs[wdir..""].delta.z}, "signs:text")
-            text:setyaw(signs[wdir..""].yaw)
+            local text = minetest.env:add_entity({x = above.x + signs[fdir].delta.x,
+                                                  y = above.y + signs[fdir].delta.y,
+                                                  z = above.z + signs[fdir].delta.z}, "signs:text")
+            text:setyaw(signs[fdir].yaw)
         end
 
         return ItemStack("")
+    end,
+    on_construct = function(pos)
+        local meta = minetest.env:get_meta(pos)
+        meta:set_string("formspec", "size[7,8]"..
+            "field[1,0;6,3;line1;;${line1}]"..
+            "field[1,1;6,3;line2;;${line2}]"..
+            "field[1,2;6,3;line3;;${line3}]"..
+            "field[1,3;6,3;line4;;${line4}]"..
+            "field[1,4;6,3;line5;;${line5}]"..
+            "field[1,5;6,3;line6;;${line6}]"..
+            "field[1,6;6,3;line7;;${line7}]")
+    end,
+    on_destruct = function(pos)
+        local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
+        for _, v in ipairs(objects) do
+            if v:get_entity_name() == "signs:text" then
+                v:remove()
+            end
+        end
+    end,
+    on_receive_fields = function(pos, formname, fields, sender)
+        local meta = minetest.env:get_meta(pos)
+        local text = {}
+        for _, v in ipairs(metas) do
+            table.insert(text, fields[v])
+            meta:set_string(v, fields[v])
+        end
+        local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
+        for _, v in ipairs(objects) do
+            if v:get_entity_name() == "signs:text" then
+                v:set_properties({textures={generate_texture(text)}})
+            end
+        end
     end,
 })
 
@@ -107,18 +119,18 @@ minetest.register_entity("signs:text", {
     on_activate = function(self)
         local meta = minetest.env:get_meta(self.object:getpos())
         local text = {}
-        for i = 1, #metas do
-            table.insert(text, meta:get_string(metas[i]))
+        for _, v in ipairs(metas) do
+            table.insert(text, meta:get_string(v))
         end
         self.object:set_properties({textures={generate_texture(text)}})
     end
 })
 
-local table_width = 110
-local table_padding = 8
+local sign_width = 110
+local sign_padding = 8
 
 generate_texture = function(lines)
-    local texture = "[combine:"..table_width.."x"..table_width
+    local texture = "[combine:"..sign_width.."x"..sign_width
     local ypos = 12
     for i = 1, #lines do
         texture = texture..generate_line(lines[i], ypos)
@@ -152,10 +164,8 @@ generate_line = function(s, ypos)
     end
     width = width - 1
 
-    print("width("..s..") = "..width)
-
     local texture = ""
-    local xpos = math.floor((table_width - 2 * table_padding - width) / 2 + table_padding)
+    local xpos = math.floor((sign_width - 2 * sign_padding - width) / 2 + sign_padding)
     for i = 1, #parsed do
         texture = texture..":"..xpos..","..ypos.."="..parsed[i]..".png"
         xpos = xpos + charwidth[parsed[i]] + 1
