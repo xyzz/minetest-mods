e74c92938f0ce80e5d2e6e99e434f68732248edc
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2012-08-10 14:38:53 +0400

    Updated signs mod: yard signs are now implemented; signs now have theirs own texture

--- signs/init.lua
+++ signs/init.lua
@@ -38,6 +38,51 @@ local signs = {
     {delta = {x = -0.399, y = 0, z = 0}, yaw = math.pi / 2},
 }
 
+local signs_yard = {
+    {delta = {x = 0, y = 0, z = -0.05}, yaw = 0},
+    {delta = {x = -0.05, y = 0, z = 0}, yaw = math.pi / -2},
+    {delta = {x = 0, y = 0, z = 0.05}, yaw = math.pi},
+    {delta = {x = 0.05, y = 0, z = 0}, yaw = math.pi / 2},
+}
+
+local sign_groups = {choppy=2, dig_immediate=2}
+
+local construct_sign = function(pos)
+    local meta = minetest.env:get_meta(pos)
+    meta:set_string("formspec", "size[7,8]"..
+        "field[1,0;6,3;line1;;${line1}]"..
+        "field[1,1;6,3;line2;;${line2}]"..
+        "field[1,2;6,3;line3;;${line3}]"..
+        "field[1,3;6,3;line4;;${line4}]"..
+        "field[1,4;6,3;line5;;${line5}]"..
+        "field[1,5;6,3;line6;;${line6}]"..
+        "field[1,6;6,3;line7;;${line7}]")
+end
+
+local destruct_sign = function(pos)
+    local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
+    for _, v in ipairs(objects) do
+        if v:get_entity_name() == "signs:text" then
+            v:remove()
+        end
+    end
+end
+
+local update_sign = function(pos, fields)
+    local meta = minetest.env:get_meta(pos)
+    local text = {}
+    for _, v in ipairs(metas) do
+        table.insert(text, fields[v])
+        meta:set_string(v, fields[v])
+    end
+    local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
+    for _, v in ipairs(objects) do
+        if v:get_entity_name() == "signs:text" then
+            v:set_properties({textures={generate_texture(text)}})
+        end
+    end
+end
+
 minetest.register_node("signs:sign", {
     description = "Sign",
     inventory_image = "default_sign_wall.png",
@@ -49,9 +94,9 @@ minetest.register_node("signs:sign", {
     drawtype = "nodebox",
     node_box = {type = "fixed", fixed = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}},
     selection_box = {type = "fixed", fixed = {-0.45, -0.15, 0.4, 0.45, 0.45, 0.498}},
-    tiles = {"default_wood.png"},
+    tiles = {"signs_top.png", "signs_bottom.png", "signs_side.png", "signs_side.png", "signs_back.png", "signs_front.png"},
     walkable = false,
-    groups = {choppy=2, dig_immediate=2},
+    groups = sign_groups,
 
     on_place = function(itemstack, placer, pointed_thing)
         local above = pointed_thing.above
@@ -60,54 +105,72 @@ minetest.register_node("signs:sign", {
                      y = under.y - above.y,
                      z = under.z - above.z}
 
-        local fdir = minetest.dir_to_facedir(dir) + 1
+        local wdir = minetest.dir_to_wallmounted(dir)
 
-        if minetest.dir_to_wallmounted(dir) < 2 then
-            --top/bottom not implemented =(
-            minetest.env:add_item(above, "signs:sign")
-        else
-            minetest.env:add_node(above, {name="signs:sign", param2 = fdir - 1})
-
-            local text = minetest.env:add_entity({x = above.x + signs[fdir].delta.x,
-                                                  y = above.y + signs[fdir].delta.y,
-                                                  z = above.z + signs[fdir].delta.z}, "signs:text")
-            text:setyaw(signs[fdir].yaw)
+        local placer_pos = placer:getpos()
+        if placer_pos then
+            dir = {
+				x = above.x - placer_pos.x,
+				y = above.y - placer_pos.y,
+				z = above.z - placer_pos.z
+			}
         end
+
+        local fdir = minetest.dir_to_facedir(dir)
+
+        local sign_info
+        if wdir == 0 then
+            --how would you add sign to ceiling?
+            minetest.env:add_item(above, "signs:sign")
+            return ItemStack("")
+        elseif wdir == 1 then
+            minetest.env:add_node(above, {name = "signs:sign_yard", param2 = fdir})
+            sign_info = signs_yard[fdir + 1]
+        else
+            minetest.env:add_node(above, {name = "signs:sign", param2 = fdir})
+            sign_info = signs[fdir + 1]
+        end
+
+        local text = minetest.env:add_entity({x = above.x + sign_info.delta.x,
+                                              y = above.y + sign_info.delta.y,
+                                              z = above.z + sign_info.delta.z}, "signs:text")
+        text:setyaw(sign_info.yaw)
 
         return ItemStack("")
     end,
     on_construct = function(pos)
-        local meta = minetest.env:get_meta(pos)
-        meta:set_string("formspec", "size[7,8]"..
-            "field[1,0;6,3;line1;;${line1}]"..
-            "field[1,1;6,3;line2;;${line2}]"..
-            "field[1,2;6,3;line3;;${line3}]"..
-            "field[1,3;6,3;line4;;${line4}]"..
-            "field[1,4;6,3;line5;;${line5}]"..
-            "field[1,5;6,3;line6;;${line6}]"..
-            "field[1,6;6,3;line7;;${line7}]")
+        construct_sign(pos)
     end,
     on_destruct = function(pos)
-        local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
-        for _, v in ipairs(objects) do
-            if v:get_entity_name() == "signs:text" then
-                v:remove()
-            end
-        end
+        destruct_sign(pos)
     end,
     on_receive_fields = function(pos, formname, fields, sender)
-        local meta = minetest.env:get_meta(pos)
-        local text = {}
-        for _, v in ipairs(metas) do
-            table.insert(text, fields[v])
-            meta:set_string(v, fields[v])
-        end
-        local objects = minetest.env:get_objects_inside_radius(pos, 0.5)
-        for _, v in ipairs(objects) do
-            if v:get_entity_name() == "signs:text" then
-                v:set_properties({textures={generate_texture(text)}})
-            end
-        end
+        update_sign(pos, fields)
+    end,
+})
+
+minetest.register_node("signs:sign_yard", {
+    paramtype = "light",
+    paramtype2 = "facedir",
+    drawtype = "nodebox",
+    node_box = {type = "fixed", fixed = {
+        {-0.45, -0.15, -0.049, 0.45, 0.45, 0.049},
+        {-0.05, -0.5, -0.049, 0.05, -0.15, 0.049}
+    }},
+    selection_box = {type = "fixed", fixed = {-0.45, -0.15, -0.049, 0.45, 0.45, 0.049}},
+    tiles = {"signs_top.png", "signs_bottom.png", "signs_side.png", "signs_side.png", "signs_back.png", "signs_front.png"},
+    walkable = false,
+    groups = {choppy=2, dig_immediate=2},
+    drop = "signs:sign",
+
+    on_construct = function(pos)
+        construct_sign(pos)
+    end,
+    on_destruct = function(pos)
+        destruct_sign(pos)
+    end,
+    on_receive_fields = function(pos, formname, fields, sender)
+        update_sign(pos, fields)
     end,
 })
 
