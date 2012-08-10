9b6bf7bc0c4489273a5c0aba1ff0807baf16f9ce
Ilya Zhuravlev <zhuravlevilya@ya.ru>
2012-08-10 19:39:30 +0400

    Updated "dropped" mod so it works with latest Minetest

--- dropped/init.lua
+++ dropped/init.lua
@@ -1,19 +1,17 @@
-math.randomseed(os.time())
-
 local function drop_inventory(player, name)
-    local items = player:inventory_get_list(name)
+    local invref = player:get_inventory()
     local pos = player:getpos()
-    for _, item in ipairs(items) do
-        if item ~= "" then
+    local size = invref:get_size(name)
+    for i = 1, size do
+        local item = invref:get_stack(name, i)
+        if item ~= nil then
             minetest.env:add_item({x = pos.x + math.random() * 2 - 1, y = pos.y, z = pos.z + math.random() * 2 - 1}, item)
+            invref:set_stack(name, i, "")
         end
     end
-    player:inventory_set_list(name, {})
 end
 
 minetest.register_on_dieplayer(function(player)
     drop_inventory(player, "main")
     drop_inventory(player, "craft")
-    -- this is quite buggy and can lead to duplicating things
-    --drop_inventory(player, "craftresult")
 end)
